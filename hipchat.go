a655193ea5b7d5a42659f98d6bd865ccba05a414
Donovan Kolbly <dkolbly@atlassian.com>
2017-03-10 10:40:50 -0600

    Make sense of html/text formats, and add URL markup to text format

--- hipchat.go
+++ hipchat.go
@@ -5,9 +5,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"html"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/dkolbly/cli"
 	"github.com/dkolbly/logging"
@@ -15,6 +17,17 @@ import (
 )
 
 var log = logging.New("hipchat-cli")
+
+var ErrPostFailed = errors.New("posting message failed")
+var ErrInsecureNotImplemented = errors.New("--insecure is not yet implemented")
+
+type Message struct {
+	From          string `json:"from,omitempty"`
+	Message       string `json:"message"`
+	Color         string `json:"color"`
+	MessageFormat string `json:"message_format"`
+	Notify        bool   `json:"notify"`
+}
 
 func main() {
 
@@ -80,6 +93,10 @@ var sendCmd = &cli.Command{
 			Aliases: []string{"k"},
 			Usage:   "Don't validate SSL credentials",
 		},
+		&cli.BoolFlag{
+			Name:  "html",
+			Usage: "input is already in HTML format; don't transform",
+		},
 	},
 }
 
@@ -107,10 +124,19 @@ func doSend(c *cli.Context) error {
 		text = string(inp)
 	}
 
+	// we always send HTML to HipChat; the difference is in how we
+	// interpret our input.  If the --html flag is specified, we
+	// pass it through unchanged.  Otherwise, we do some basic
+	// processing
+	format := "html"
+	if !c.Bool("html") {
+		text = string(processPlainText([]byte(text)))
+	}
+
 	msg := &Message{
 		Color:         c.String("color"),
 		Message:       text,
-		MessageFormat: "html",
+		MessageFormat: format,
 		Notify:        c.Bool("notify"),
 	}
 	if c.IsSet("from") {
@@ -129,14 +155,20 @@ func doSend(c *cli.Context) error {
 
 	req.Header.Set("Content-Length", fmt.Sprintf("%d", len(buf)))
 	req.Header.Set("Content-Type", "application/json")
-	auth := fmt.Sprintf("Bearer %s", token)
-	req.Header.Set("Authorization", auth)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	if c.Bool("insecure") {
+		// TODO implement --insecure; it requires setting up a
+		// http.Transport with an appropriate TLSClientConfig
+		return ErrInsecureNotImplemented
+	}
 
 	rsp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rsp.Body.Close()
+
 	entity, err := ioutil.ReadAll(rsp.Body)
 	if err != nil {
 		log.Fatalf("Could not read response entity: %s", err)
@@ -157,12 +189,46 @@ func doSend(c *cli.Context) error {
 	return nil
 }
 
-var ErrPostFailed = errors.New("posting message failed")
+// urlRe is a modified form(*) of the @gruber v2 URL regex from
+// https://mathiasbynens.be/demo/url-regex, which seems to be a good
+// compromise between complexity and completeness (leaning towards
+// fail "safe" where safe is defined as recognizing URLs).
+//
+// Also, note that this will successfully match only single balanced
+// parentheticals which in this context is a desirable property
+// because of the likelihood that someone writing something like "Hey
+// @bob did you see the thing? (you know, http://bit.ly/thing)" does
+// not intend the ')' to be part of the URL.  However, it is
+// limited(**) in capability
+//
+// (** this StackOverflow answer, my all-time favorite, explains one
+// reason that it is limited, although in the context of HTML parsing
+// and not URL parsing: http://bit.ly/1hY5QfK)
+//                                           ^ see, I just did it
+//
 
-type Message struct {
-	From          string `json:"from,omitempty"`
-	Message       string `json:"message"`
-	Color         string `json:"color"`
-	MessageFormat string `json:"message_format"`
-	Notify        bool   `json:"notify"`
+var urlRe = regexp.MustCompile(`(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%]))(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s` + "`" + `!()\[\]{};:'".,<>?«»“”‘’]))`)
+
+// processPlainText takes a message in plain text as input and applies
+// certain transformations to HTML-ify it
+func processPlainText(src []byte) []byte {
+
+	out := &bytes.Buffer{}
+
+	plain := func(chunk []byte) {
+		out.WriteString(html.EscapeString(string(chunk)))
+	}
+
+	for {
+		loc := urlRe.FindIndex(src)
+		if loc == nil {
+			plain(src)
+			break
+		}
+		plain(src[:loc[0]])
+		url := src[loc[0]:loc[1]]
+		fmt.Fprintf(out, `<a href="%s">%s</a>`, url, url)
+		src = src[loc[1]:]
+	}
+	return out.Bytes()
 }
